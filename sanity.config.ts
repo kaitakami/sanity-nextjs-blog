ed9ec0c49dd1e7fdecfbd9e0c00effe379f0a03f
Kai Takami <kaitakami@protonmail.com>
2023-01-01 09:36:51 +0900

    Config styles in sanity panel with theme

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,10 @@
 import { visionTool } from '@sanity/vision'
-import { defineConfig } from 'sanity'
+import { defineConfig, StudioLogo } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { schemaTypes } from './schemas'
+import { myTheme } from './theme'
+import Logo from './components/Logo'
+import StudioNavbar from './components/StudioNavbar'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
@@ -15,5 +18,12 @@ export default defineConfig({
   plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
-  }
+  },
+  studio: {
+    components: {
+      logo: Logo,
+      navbar: StudioNavbar,
+    }
+  },
+  theme: myTheme
 })
